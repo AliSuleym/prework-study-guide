91ba89bd2049c2d149f7d708e9bc38b432a906a1
https://github.com/AliSuleym <ams.ma@hotmail.com>
2022-11-09 15:19:35 +0000

    upadatet

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
-var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+
+/*
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -11,3 +16,4 @@ if (topic === 'HTML') {
 } else {
  console.log('Please try again!');
 }
+*/
