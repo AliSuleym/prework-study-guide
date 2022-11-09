8ec96fb739ce6e085b1d8a3c8dca51c7cdaf6ab3
https://github.com/AliSuleym <ams.ma@hotmail.com>
2022-11-09 16:04:08 +0000

    upadated

--- Javascript.js
+++ Javascript.js
@@ -1,20 +1,13 @@
-ar topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
-function listTopics() {
- for (var x = 0; x < topics.length; x++) {
-   console.log(topics[x]);
- }
-}
-
 function selectTopic() {
- if (topic === 'HTML') {
-   console.log("Let's study HTML!");
- } else if (topic === 'CSS') {
-   console.log("Let's study CSS!");
- } else if (topic === 'Git') {
-   console.log("Let's study Git!");
- } else if (topic === 'JavaScript') {
-   console.log("Let's study JavaScript!");
- } else {
-   console.log('Please try again!');
- }
+    if (topics === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (topics === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (topics === 'Git') {
+      console.log("Let's study Git!");
+    } else if (topics === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
